e1210ce90c764997ed6dc294f0d7c6a4b9d7f01d
JustinLeeVance <justinleevance94@gmail.com>
2023-12-12 12:41:54 -0500

    ArrowFuctions

---
+++ index.js
@@ -0,0 +1,16 @@
+let a = 2000;
+let b = 100;
+let newNum;
+const divide = (a , b) => newNum = a/b 
+
+const square = num => num**2
+
+
+//I am creating an arrow function that takes in two parameters
+//The result/return value of the arrow function is stored in
+//variable add, which can be called upon at anypoint in the program
+
+const add = (num1, num2) => num1 + num2
+
+const newNew = add(1,5);
+console.log(newNew)
